0423703c55fb48d882806c01c4d607e66634dd7b
derekhodge <derekrhodge@gmail.com>
2019-02-26 21:37:49 -0700

    started reader method; added batch class

--- src/main/java/kitchen/Main.java
+++ src/main/java/kitchen/Main.java
@@ -1,7 +1,58 @@
 package kitchen;
 
+import java.io.File;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
-	public static void main(String[] args) {
 	
+	private static final Logger LOG = LogManager.getLogger(Main.class);
+	
+	
+	String inputDir = "C:\\temp\\kitchen\\input";
+	String outputDir = "C:\\temp\\kitchen\\output";
+	
+	public void receiveOrder() {
+		File directory = new File(inputDir);
+		File[] directoryListing = directory.listFiles();
+	    for (File inputFile : directoryListing) {
+	    	
+	      }
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.receiveOrder();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/kitchen/Oven.java
+++ src/main/java/kitchen/Oven.java
@@ -1,7 +1,12 @@
 package kitchen;
 
 public class Oven {
-		public void bake() {
+		public void bake(Pastry pastry) {
+			
+				double butter = pastry.getButterWeight();
+				double flour = pastry.getFlourWeight();
+				double salt = pastry.getSaltWeight();
+				double water = pastry.getWaterWeight();
 			
 		}
 }
